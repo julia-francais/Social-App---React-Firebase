397bc941d9e231af5cb40d071591b901fe1f885a
julia-francais <ju.francais@gmail.com>
2020-03-14 21:32:29 +0100

    add login route and validation

---
+++ dbSchema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "scream body",
+      createdAt: "2020-03-14T09:43:42.986Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
