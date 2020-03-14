11c292e5f89bb6bda27844fdfcd0a3bad1417be3
julia-francais <ju.francais@gmail.com>
2020-03-14 21:40:10 +0100

    config added

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,16 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
+config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+};
+
 const app = require("express")();
 
 const firebase = require("firebase");
